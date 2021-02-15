a10d462e373c3749210dc1f551abe58f86faade2
Nicolas <n.barile.57@gmail.com>
2021-02-15 11:36:12 +0100

    Création de la classe du Projet

---
+++ src/tsp/projects/BarileKorpisProjet.java
@@ -0,0 +1,25 @@
+package tsp.projects;
+
+import tsp.evaluation.Evaluation;
+
+public class BarileKorpisProjet extends Project{
+    /**
+     * Constructeur
+     *
+     * @param evaluation Méthode d'évaluation de la solution
+     * @throws InvalidProjectException
+     */
+    public BarileKorpisProjet(Evaluation evaluation) throws InvalidProjectException {
+        super(evaluation);
+    }
+
+    @Override
+    public void initialization() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
